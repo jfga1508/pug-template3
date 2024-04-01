e2fd88bde5764b49259cf992bd3c91ee23f5ad78
Jean Francois Gonzalez <jgonzaleza@ultragroupla.com>
2024-04-01 15:36:14 -0500

    Feature(add): Added the last bit of code to complete the landing

--- site/www_mk/assets/js/app.js
+++ site/www_mk/assets/js/app.js
@@ -1,3 +1,42 @@
+$window = $(window);
+$gallery_slider = $('.product-gallery');
+settings = {
+  infinite: true,
+  arrows: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000
+};
+
+if ($window.width() <= 991) {
+  $gallery_slider.slick(settings);
+}
+
+const slickHeight = $(".slick-track").outerHeight();
+
+const slideHeight = $(".slick-track").find(".slick-slide").outerHeight();
+
+function resizeSlider() {
+  $(".slick-track")
+    .find(".slick-slide .slide-wrap")
+    .css("height", slickHeight + "px");
+}
+
+function increment() {
+  const input = $("#amount");
+  const current = parseInt(input.val());
+  const increment = current + 1;
+  input.val(increment);
+}
+
+function decrement() {
+  const input = $("#amount");
+  const current = parseInt(input.val());
+  const decrement = current <= 1 ? current : current - 1;
+  input.val(decrement);
+}
+
 $(".multiple-items")
   .slick({
     infinite: true,
@@ -21,7 +60,7 @@ $(".multiple-items")
           slidesToShow: 3,
           slidesToScroll: 3,
         },
-      }
+      },
     ],
   })
   .on("setPosition", function () {
@@ -30,14 +69,11 @@ $(".multiple-items")
 
 $(window).on("resize", function (e) {
   resizeSlider();
+  if ($window.width() > 990) {
+    if ($gallery_slider.hasClass("slick-initialized"))
+      $gallery_slider.slick("unslick");
+    return;
+  }
+  if (!$gallery_slider.hasClass("slick-initialized"))
+    return $gallery_slider.slick(settings);
 });
-
-var slickHeight = $(".slick-track").outerHeight();
-
-var slideHeight = $(".slick-track").find(".slick-slide").outerHeight();
-
-function resizeSlider() {
-  $(".slick-track")
-    .find(".slick-slide .slide-wrap")
-    .css("height", slickHeight + "px");
-}
